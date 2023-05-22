f4d57c61936c0cadc69516495751203416f5ee7e
clvnkhr <calvin_khor@hotmail.com>
2023-05-22 14:45:53 +0800

    OS checking functionality

--- lua/macaltkey/init.lua
+++ lua/macaltkey/init.lua
@@ -1,80 +1,120 @@
 local M = {}
--- TODO: OS detector + potential to set both keymaps (name?) and ability to override
+
+-- taken from f-person/auto-dark-mode.nvim
+---@return 'win'|'darwin'|'linux'
+M.get_os = function()
+	if package.config:sub(1, 1) == "\\" then
+		return "win"
+	elseif (io.popen("uname -s"):read("*a")):match("Darwin") then
+		return "darwin"
+	else
+		return "linux"
+	end
+end
+
+-- Call this function before using the plugin. Sets the dict, modifier, double_set, and gets the os.
+-- supported options:
+-- language = "en-GB" | "en-US", defaults to "en-GB"
+-- modifier = str, defaults to "a" for alt
+-- double_set = bool, defaults to false
 M.setup = function(opts)
 	opts = opts or {}
 	M.language = opts.language or "en-GB"
 	M.dict = require('macaltkey.dicts')[M.language]
-	if not opts.convenience then M.convenience = true end
 	M.modifier = opts.modifier or 'a'
 	M.double_set = opts.double_set or false
+	M.os = M.get_os()
 end
 
 local lowerupper = function(modifier)
 	modifier = modifier or M.modifier
 	return string.lower(modifier) .. string.upper(modifier)
 end
+
+-- makes a lua pattern using modifier
 M.pattern = function(modifier)
 	modifier = modifier or M.modifier
 	return "<[" .. lowerupper(modifier) .. "]--(.)>"
 end
 
+-- function to be passed into string.gsub.
 M.replacer = function(match)
 	return M.dict[match] or ("<" .. M.modifier .. "-" .. match .. ">")
 end
 
+-- Do nothing if not using Mac OS
+-- Otherwise, replace using M.pattern and M.replacer
 M.convert = function(lhs, modifier, replacer)
+	if M.os ~= "darwin" then return lhs end
+
 	local out = string.gsub(lhs, M.pattern(modifier), replacer or M.replacer)
+
 	if M.language == 'en-GB' then
 		-- INFO: additional handling required for the £ character.
 		out = string.gsub(out, '<[' .. lowerupper(modifier) .. ']--(\194\163)>', M.dict)
 	end
 	return out
 end
+
+-- convenience functions
 M.keymap = {
-	set = function(mode, lhs, rhs, opts)
+	-- simple wrapper around vim.keymap.set
+	set = function(mode, lhs, rhs, opts, opts2)
 		opts = opts or {}
+		opts2 = opts2 or {}
 		vim.keymap.set(mode, M.convert(lhs), rhs, opts)
-		if opts.double_set or M.double_set then
+		if opts2.double_set == true or (opts2.double_set == nil and M.double_set == true) then
 			vim.keymap.set(mode, lhs, rhs, opts)
 		end
 	end,
-	del = function(mode, lhs, opts)
+	-- simple wrapper around vim.keymap.del
+	del = function(mode, lhs, opts, opts2)
 		opts = opts or {}
+		opts2 = opts2 or {}
 		vim.keymap.del(mode, M.convert(lhs), opts)
-		if opts.double_set or M.double_set then
+		if opts2.double_set == true or (opts2.double_set == nil and M.double_set == true) then
 			vim.keymap.del(mode, lhs, opts)
 		end
 	end
 }
 
-M.nvim_set_keymap = function(mode, lhs, rhs, opts)
+-- simple wrapper around vim.api.nvim_set_keymap.
+M.nvim_set_keymap = function(mode, lhs, rhs, opts, opts2)
 	opts = opts or {}
+	opts2 = opts2 or {}
 	vim.api.nvim_set_keymap(mode, M.convert(lhs), rhs, opts)
-	if opts.double_set or M.double_set then
+	-- P(opts2.double_set)
+	-- P((opts2.double_set == nil) and (M.double_set == true))
+	if (opts2.double_set == true) or ((opts2.double_set == nil) and (M.double_set == true)) then
+		-- P('inside if')
 		vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 	end
 end
 
-M.nvim_buf_set_keymap = function(buffer, mode, lhs, rhs, opts)
+-- simple wrapper around vim.api.nvim_buf_set_keymap
+M.nvim_buf_set_keymap = function(buffer, mode, lhs, rhs, opts, opts2)
 	opts = opts or {}
+	opts2 = opts2 or {}
 	vim.api.nvim_buf_set_keymap(buffer, mode, M.convert(lhs), rhs, opts)
-	if opts.double_set or M.double_set then
+	if opts2.double_set == true or (opts2.double_set == nil and M.double_set == true) then
 		vim.api.nvim_buf_set_keymap(buffer, mode, lhs, rhs, opts)
 	end
 end
 
+-- simple wrapper around vim.api.nvim_del_keymap
 M.nvim_del_keymap = function(mode, lhs, opts)
 	opts = opts or {}
 	vim.api.nvim_del_keymap(mode, M.convert(lhs))
-	if opts.double_set or M.double_set then
+	if opts.double_set == true or (opts.double_set == nil and M.double_set == true) then
 		vim.api.nvim_del_keymap(mode, lhs)
 	end
 end
 
+-- simple wrapper around vim.api.nvim_buf_del_keymap
 M.nvim_buf_del_keymap = function(buffer, mode, lhs, opts)
 	opts = opts or {}
 	vim.api.nvim_buf_del_keymap(buffer, mode, M.convert(lhs))
-	if opts.double_set or M.double_set then
+	if opts.double_set == true or (opts.double_set == nil and M.double_set == true) then
 		vim.api.nvim_buf_del_keymap(buffer, mode, lhs)
 	end
 end
--- tests/macaltkey_spec.lua
+++ tests/macaltkey_spec.lua
@@ -1,5 +1,6 @@
 local mak = require("macaltkey")
 
+-- taken from teejdv and bash's video
 local find_map = function(maps, lhs)
 	for _, map in ipairs(maps) do
 		if map.lhs == lhs then
@@ -8,22 +9,27 @@ local find_map = function(maps, lhs)
 	end
 end
 
+local printmaplhs = function(maps)
+	for _, map in ipairs(maps) do
+		print(map.lhs)
+	end
+end
+
+
 describe("macaltkey", function()
-	it("can correctly maps chars with en-US dict", function()
+	it("can correctly map chars with en-US dict", function()
 		mak.setup({ language = "en-US" })
-		print(mak.language)
 		local hand_typed_string =
 		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`,./;\'\\[]~<>?:"|{}1234567890-=!@#$%^&*()_+'
 		local hand_typed_expected =
 		'å∫ç∂´ƒ©˙ˆ∆˚¬µ˜øπœ®ß†¨√∑≈¥ΩÅıÇÎ´Ï˝ÓˆÔÒÂ˜Ø∏Œ‰Íˇ¨◊„˛Á¸`≤≥÷…æ«“‘`¯˘¿ÚÆ»”’¡™£¢∞§¶•ªº–≠⁄€‹›ﬁﬂ‡°·‚—±'
-		local replaced = string.gsub(string.gsub(hand_typed_string, '.', mak.dict), '\194\163', mak.dict)
-		-- NOTE: the double gsub is required to deal with £ which is not a signle 8-byte char.
+		local replaced = string.gsub(hand_typed_string, '.', mak.dict)
 
 		assert.are.same(hand_typed_expected, replaced)
 	end
 	)
 
-	it("can correctly maps chars with en-GB dict", function()
+	it("can correctly map chars with en-GB dict", function()
 		mak.setup({ language = "en-GB" })
 		local hand_typed_string = -- NOTE: the # symbol is typed via opt-3. shift-3 gives £.
 		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`,./;\'\\[]~<>?:"|{}1234567890-=!@£$%^&*()_+'
@@ -38,22 +44,30 @@ describe("macaltkey", function()
 
 	it("can convert legal commands", function()
 		mak.setup({ language = "en-GB" })
+		mak.os = "darwin" --INFO: allow tests to pass on non-macs
+
 		assert.are.same("hello å‹ß<leader>", mak.convert("hello <a-a><a-£><A-s><leader>"))
 	end
 	)
+
 	it("won't convert illegal commands", function()
 		mak.setup({ language = "en-GB" })
+		mak.os = "darwin"
+
 		assert.are.same("<a-⦿>", mak.convert("<a-⦿>"))
 	end
 	)
-	-- TODO: test the setter and dellers
-	--
+
 	it("can set commands", function()
 		mak.setup({ language = "en-GB" })
+		mak.os = "darwin"
 
-		mak.nvim_set_keymap("n", "test1<a-a>", "rhs1", {})
-		mak.keymap.set("n", "test2<a-b>", "rhs2", {})
-		-- TODO: I don't know how nvim_buf_set_keymap works but it is similarly wrapped so should also work
+		mak.nvim_set_keymap("n", "test1<a-a>", "rhs1")
+		mak.keymap.set("n", "test2<a-b>", "rhs2")
+		-- WARN: I don't know how nvim_buf_set_keymap works
+		-- but it is similarly wrapped, so should also work if this test passes
+		-- similarly with del below
+
 		local maps = vim.api.nvim_get_keymap("n")
 
 		local found1 = find_map(maps, mak.convert("test1<a-a>"))
@@ -61,6 +75,15 @@ describe("macaltkey", function()
 
 		assert.are.same("rhs1", found1.rhs)
 		assert.are.same("rhs2", found2.rhs)
+		-- INFO: verify that extra commands are not set
+		local notfound1 = find_map(maps, "test1<M-a>")
+		local notfound2 = find_map(maps, "test2<M-b>")
+		assert.are.same(nil, notfound1)
+		assert.are.same(nil, notfound2)
+		--INFO: cleanup for next test
+		mak.keymap.del("n", "test1<a-a>")
+		mak.keymap.del("n", "test2<a-b>")
+		maps = vim.api.nvim_get_keymap("n")
 	end
 	)
 	it("can del commands", function()
@@ -73,7 +96,7 @@ describe("macaltkey", function()
 		local found1 = find_map(maps, mak.convert("test1<a-a>"))
 
 		local found2 = find_map(maps, mak.convert("test2<a-b>"))
-		assert.are.same("rhs1", found1.rhs)
+		assert.are.same("rhs1", found1.rhs) -- INFO: verify they exist before deleting
 		assert.are.same("rhs2", found2.rhs)
 
 		mak.keymap.del("n", "test1<a-a>")
@@ -84,6 +107,78 @@ describe("macaltkey", function()
 		found2 = find_map(maps, mak.convert("test2<a-b>"))
 		assert.are.same(nil, found1)
 		assert.are.same(nil, found2)
+	end
+	)
+	-- TODO: test double_set and what happens if OS is not set. And actually run the mac test
+	it("can double set keymaps when option passed to function", function()
+		mak.setup({ language = "en-GB" })
+		mak.os = "darwin"
+
+		mak.nvim_set_keymap("n", "test1<a-a>", "rhs1", {}, { double_set = true })
+		mak.keymap.set("n", "test2<a-b>", "rhs2", {}, { double_set = true })
+
+		local maps = vim.api.nvim_get_keymap("n")
+
+		local found1a = find_map(maps, mak.convert("test1<a-a>"))
+		-- NOTE: <a-x> is converted to <M-x>
+		local found1b = find_map(maps, "test1<M-a>")
+		local found2a = find_map(maps, mak.convert("test2<a-b>"))
+		local found2b = find_map(maps, "test2<M-b>")
+		assert.are.same("rhs1", found1a.rhs)
+		assert.are.same("rhs1", found1b.rhs)
+		assert.are.same("rhs2", found2a.rhs)
+		assert.are.same("rhs2", found2b.rhs)
+
+		--INFO: cleanup for next test
+		mak.keymap.del("n", "test1<a-a>", {}, { double_set = true })
+		mak.keymap.del("n", "test2<a-b>", {}, { double_set = true })
+	end
+	)
+
+	it("can double set keymaps when option passed to setup", function()
+		mak.setup({ language = "en-GB", double_set = true })
+		mak.os = "darwin"
+
+		mak.nvim_set_keymap("n", "test1<a-a>", "rhs1")
+		mak.keymap.set("n", "test2<a-b>", "rhs2")
+
+		local maps = vim.api.nvim_get_keymap("n")
+		local found1a = find_map(maps, mak.convert("test1<a-a>"))
+		-- NOTE: <a-x> is converted to <M-x>
+		local found1b = find_map(maps, "test1<M-a>")
+		local found2a = find_map(maps, mak.convert("test2<a-b>"))
+		local found2b = find_map(maps, "test2<M-b>")
+		assert.are.same("rhs1", found1a.rhs)
+		assert.are.same("rhs1", found1b.rhs)
+		assert.are.same("rhs2", found2a.rhs)
+		assert.are.same("rhs2", found2b.rhs)
+
+		--INFO: cleanup for next test
+		mak.keymap.del("n", "test1<a-a>", {}, { double_set = true })
+		mak.keymap.del("n", "test2<a-b>", {}, { double_set = true })
+	end
+	)
+
+	it("won't double set keymaps when option passed to function overrides setup", function()
+		mak.setup({ language = "en-GB", double_set = true })
+		mak.os = "darwin"
+
+		mak.nvim_set_keymap("n", "test1<a-a>", "rhs1", {}, { double_set = false })
+		mak.keymap.set("n", "test2<a-b>", "rhs2", {}, { double_set = false })
+		local maps = vim.api.nvim_get_keymap("n")
+		-- printmaplhs(maps)
+		local found1a = find_map(maps, mak.convert("test1<a-a>"))
+		-- NOTE: <a-x> is converted to <M-x>
+		local found1b = find_map(maps, "test1<M-a>")
+		local found2a = find_map(maps, mak.convert("test2<a-b>"))
+		local found2b = find_map(maps, "test2<M-b>")
+		assert.are.same("rhs1", found1a.rhs)
+		assert.are.same(nil, found1b)
+		assert.are.same("rhs2", found2a.rhs)
+		assert.are.same(nil, found2b)
+		--INFO: cleanup for next test
+		mak.keymap.del("n", "test1<a-a>", {}, { double_set = false })
+		mak.keymap.del("n", "test2<a-b>", {}, { double_set = false })
 	end
 	)
 end
